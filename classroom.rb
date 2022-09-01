6ff458c9db0723036a88031d894921ec8c5245a3
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-01 14:21:54 +0300

    Added book and rental relationship

--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,13 @@
 class Classroom
-    attr_accessor :label
-    def initialize(label)
-        @label=label
-    end
+  attr_accessor :label
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_a_student(student)
+    @students << student
+    student.classroom = self
+  end
 end
