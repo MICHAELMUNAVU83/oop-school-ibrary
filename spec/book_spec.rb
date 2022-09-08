fabccb20efabb5803d43cca687c9539dab2ba068
Shubh <shubh.scb@gmail.com>
2022-09-08 13:26:43 +0530

    TEST PASS

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -14,3 +14,16 @@ describe Book do
     expect(@book.author).to eq 'Nelson'
   end
 end
+
+describe Book do
+  
+  before :each do
+    @book = Book.new nil, 'Harry Potter', 'J.K. Rowling'
+  end
+  it 'should print the book title' do
+    expect(@book.title).to eq 'Harry Potter'
+  end
+  it 'should print the book author' do
+    expect(@book.author).to eq 'J.K. Rowling'
+  end
+end
