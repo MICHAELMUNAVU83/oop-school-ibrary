3a3dbbab60203e6002f682453cb67fec5ab80071
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-08 10:47:39 +0300

    Added first tests

--- app.rb
+++ app.rb
@@ -18,6 +18,6 @@ class App
       @options.save_people_to_file
       @options.save_rental_data
     end
-    puts 'thank you for using the application'
+    puts 'thank you for using the app'
   end
 end
